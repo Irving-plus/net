b5e7b344d3130c30b6d751206f4bbf50d74d9118
周希来 <742042932@qq.com>
2020-02-29 16:13:25 +0800

    11

--- com-version-net-core/src/main/java/com/version/common/util/SpringContextUtils.java
+++ com-version-net-core/src/main/java/com/version/common/util/SpringContextUtils.java
@@ -16,8 +16,8 @@
 
 package com.version.common.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -29,8 +29,9 @@ import org.springframework.stereotype.Component;
  * 
  */
 @Component
+@Slf4j
 public class SpringContextUtils implements ApplicationContextAware {
-	private static final Logger log = LoggerFactory.getLogger(SpringContextUtils.class);
+
 	public static ApplicationContext applicationContext; 
 	
 	
